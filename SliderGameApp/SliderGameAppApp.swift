ac55150e4caddd959c7b6e3deed87ad44525b34f
Anton Zarichny <antonzarichny@gmail.com>
2022-10-28 17:17:02 +0300

    Initial Commit

---
+++ SliderGameApp/SliderGameAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderGameAppApp.swift
+//  SliderGameApp
+//
+//  Created by Антон Заричный on 28.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct SliderGameAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
